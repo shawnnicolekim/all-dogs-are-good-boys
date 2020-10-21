e5c3e49a9d48e4429efd74c4cc3d1d6453a1909f
Shawn Kim <shawnnicolekim@gmail.com>
2020-10-21 13:44:50 -0700

    Progress on GET for comments of a post. Further questions on it I want to ask mentor so I am moving onto POST call to create a new post.

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const { db } = require('../database/connect.js');
 
 app.use(express.json());
 
-// GET all posts
-// gets the 10 most recent posts
+// GET the 10 most recent posts
+// DONE
 app.get('/', (req, res) => {
-  let queryString = 'SELECT * FROM posts ORDER BY timestamp DESC LIMIT 10';
-  db.many(queryString)
+  let query = 'SELECT * FROM posts ORDER BY timestamp DESC LIMIT 10';
+  db.many(query)
     .then(data => {
       res.json(data);
     })
@@ -17,16 +17,51 @@ app.get('/', (req, res) => {
     })
 })
 
+// POSTS a new post
+// IN PROGRESS
+app.post('/post', (req, res) => {
+  let query = 'INSERT INTO posts(user_id, timestamp, image, caption, votes) VALUES()';
+  db.none(query)
+})
+
 // GET all comments of a specific post
-app.get('comments/:postid', (req, res) => {
-  console.log('req is :', req);
+// IN PROGRESS
+app.get('/comments/:postid', (req, res) => {
+  var postid = req.params.postid;
 
-  var postId = req.query.postId;
-  console.log('postId is: ', postId);
+  var query = `SELECT * FROM comments_posts WHERE post_id=${postid} ORDER BY timestamp DESC LIMIT 10`;
 
-  var queryString = `SELECT * FROM comments ORDER BY timestamp DESC WHERE post_id=${postId} LIMIT 10`;
+  db.many(query)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.error('Could not get the comments of given post')
+    })
 
+/*
+Using postid === '4', I get these 2 objects. How should I go about getting the correct username and comment strings? Do I need to iterate over the array and go through each user_id and comment_id?
 
+From the user, I need username and profile pic.
+From the comment, I just need text.
+
+[
+    {
+        "id": 19,
+        "timestamp": "2016-11-11T14:54:36.259Z",
+        "user_id": 71,
+        "comment_id": 2,
+        "post_id": 4
+    },
+    {
+        "id": 3,
+        "timestamp": "2012-05-17T19:12:48.942Z",
+        "user_id": 22,
+        "comment_id": 17,
+        "post_id": 4
+    }
+]
+*/
 })
 
 app.listen(3000, () => {
