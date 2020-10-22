e2c061c501a07e4169445ceab1c3ce3ef0f48f3f
Shawn Kim <shawnnicolekim@gmail.com>
2020-10-21 20:58:43 -0700

    Initializing App. Need to figure out webpack/babel for react.

---
+++ client/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state: {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        There's nothing here yet.
+      </div>
+    )
+  }
+}
+
+export default App;
