f6da14b833a60361664690603616bfa58fbed188
Shawn Kim <shawnnicolekim@gmail.com>
2020-10-28 10:58:12 -0700

    Edited Signup to show labels and line breaks.

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,41 +1,55 @@
 import React, { useState } from 'react';
 
 const Signup = () => {
-  const [username, setUsername] = useState('');s
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  /*
-  const usernameChange = (event) => {
-    setUsername(event.target.value)
-  }
+  const handleInputChange = (event) => {
+    let { value, name } = event.target;
 
-  const emailChange = (event) => {
-    setEmail(event.target.value)
-  }
-
-  const passwordChange = (event) => {
-    setPassword(event.target.value)
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
   }
 
   const handleSubmit = () => {
-    axios.post('/signup')
+    axios.post('/signup', {
+      username,
+      email,
+      password
+    })
+    .then((data) => {
+      console.log('data from signup post');
+    })
   }
-  */
+
 
   return (
     <div id='signup'>
       <h1>Signup</h1>
       <div>All inputs are required.</div>
-      <form action='/signup' method='POST'>
-        <label for='username'></label>
+      <form onSubmit={handleSubmit}>
+        <label for='username'>Username</label>
         <input type='text' id='username' name='username'></input>
-        <label for='email'></label>
+        <br></br>
+
+        <label for='email'>Email</label>
         <input type='text' id='email' name='email'></input>
-        <label for='password'></label>
+        <br></br>
+
+        <label for='password'>Password</label>
         <input type='text' id='password' name='password'></input>
-        <label for='avatar'></label>
-        <input type='text' id='avatar' name='avatar'></input>
+        <br></br>
+
+        <label for='avatar'>Profile Pic </label>
+        <input type='file' id='avatar' name='avatar'></input>
+        <br></br>
+
         <input type='submit' value='Signup'></input>
       </form>
       <a href='./Login.jsx'>Go to Login Page</a>
